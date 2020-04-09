740fe68faab79e44976477f945e14bdac4ba86fd
Dennise <63337204+dentolos19@users.noreply.github.com>
2020-04-09 19:23:15 +0800

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace SecurePad
+{
+
+    public partial class App
+    {
+
+        private void Initialize(object sender, StartupEventArgs e)
+        {
+
+        }
+
+    }
+
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("SecurePad")]
+[assembly: AssemblyDescription("A Private Notepad For Private Users")]
+[assembly: AssemblyCompany("Dennise")]
+[assembly: AssemblyProduct("SecurePad")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
